e6aa8f35b49a51c0127544c7b208c8ca84b6e0e6
Justin Linton <127802772+aberrator9@users.noreply.github.com>
2023-08-03 18:12:55 -0400

    Add gitignore. Rename sln minus dash.

--- Pages/Services/DataAccessService.cs
+++ Pages/Services/DataAccessService.cs
@@ -1,23 +1,27 @@
 using System;
-using System.Data.SqlTypes;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
 using System.Linq;
-using Dapper;
 using D2R_Items.Models;
 
 namespace D2R_Items.Services;
+
+public class ItemContext : DbContext
+{
+    public DbSet<Weapon> Weapons { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(
+            @"Server=(localdb)\mssqllocaldb;Database=D2R;Trusted_Connection=True");
+    }
+}
 
 public class DataAccessService
 {
     public IEnumerable<Weapon>? Query(string query)
     {
-        // string sql = "SELECT TOP 10 * FROM OrderDetails";
-
-        // using (var connection = new SqlConnection(FiddleHelper.GetConnectionStringSqlServerW3Schools()))
-        // {
-        //     var orderDetail = connection.Query(sql).FirstOrDefault();
-
-        //     FiddleHelper.WriteTable(orderDetail);
-        // }
-        // return null;
+        return null;
     }
 }
