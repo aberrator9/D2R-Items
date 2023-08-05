b68bddd5c26faa47d86d70f2521b8fb84f408ed3
Justin Linton <127802772+aberrator9@users.noreply.github.com>
2023-08-04 22:49:56 -0400

    Add PopulateData class.

---
+++ Models/PopulateData.cs
@@ -0,0 +1,64 @@
+﻿using D2RItems.Data;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Diagnostics;
+
+namespace D2RItems.Models;
+
+public class PopulateData
+{
+    public PopulateData(IWebHostEnvironment webHostEnvironment)
+    {
+        WebHostEnvironment = webHostEnvironment;
+    }
+
+    public IWebHostEnvironment WebHostEnvironment { get; }
+
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        Debug.WriteLine("In initialize");
+
+        using (var context = new D2RItemsContext(
+            serviceProvider.GetRequiredService<
+                DbContextOptions<D2RItemsContext>>()))
+        {
+            if (context == null || context.Weapon == null)
+            {
+                throw new ArgumentNullException("Null D2RItemsContext");
+            }
+
+            if (context.Weapon.Any())
+            {
+                return;
+            }
+
+            // read JSON directly from a file
+            using (StreamReader file = File.OpenText(@"c:\users\qbdth\repos\d2r-items\wwwroot\data\weapons.json"))
+            using (JsonTextReader reader = new JsonTextReader(file))
+            {
+                Debug.WriteLine("File was loaded" + file);
+                JObject o2 = (JObject)JToken.ReadFrom(reader);
+                foreach (var j in o2)
+                {
+                    Debug.WriteLine(j);
+                }
+            }
+
+            //context.Weapon.AddRange(
+
+            //    // Map each parameter to a new Weapon
+            //    new Weapon()
+            //    {
+            //        Name = "The Crungis",
+            //        Tier = "Elite",
+            //        Type = "Boulder",
+            //        Sockets = 5,
+            //        OneHandDmg = 15,
+            //        TwoHandDmg = 150
+            //    }
+            //    );
+            //context.SaveChanges();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,22 +1,32 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using D2RItems.Data;
+using D2RItems.Models;
+using System.Diagnostics;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<D2RItemsContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("D2RItemsContext") ?? throw new InvalidOperationException("Connection string 'D2RItemsContext' not found.")));
+	options.UseSqlServer(builder.Configuration.GetConnectionString("D2RItemsContext") ?? throw new InvalidOperationException("Connection string 'D2RItemsContext' not found.")));
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+	var services = scope.ServiceProvider;
+
+	Debug.WriteLine("Before disposable service");
+	PopulateData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	app.UseExceptionHandler("/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
 }
 
 app.UseHttpsRedirection();
