e5ef25ec46cb448937f6c94719d6015c24612878
Justin Linton <127802772+aberrator9@users.noreply.github.com>
2023-08-07 16:47:03 -0400

    Add weapons page, initial data population, type name aliases.

---
+++ Migrations/20230807180353_02.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace D2RItems.Migrations
+{
+    /// <inheritdoc />
+    public partial class _02 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Speed",
+                table: "Weapon",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Speed",
+                table: "Weapon");
+        }
+    }
+}
--- Models/PopulateData.cs
+++ Models/PopulateData.cs
@@ -1,5 +1,6 @@
 ﻿using D2RItems.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.Diagnostics;
@@ -8,57 +9,80 @@ namespace D2RItems.Models;
 
 public class PopulateData
 {
-    public PopulateData(IWebHostEnvironment webHostEnvironment)
-    {
-        WebHostEnvironment = webHostEnvironment;
-    }
+	public PopulateData(IWebHostEnvironment webHostEnvironment)
+	{
+		WebHostEnvironment = webHostEnvironment;
+	}
 
-    public IWebHostEnvironment WebHostEnvironment { get; }
+	public IWebHostEnvironment WebHostEnvironment { get; }
 
-    public static void Initialize(IServiceProvider serviceProvider)
-    {
-        Debug.WriteLine("In initialize");
+	public static void Initialize(IServiceProvider serviceProvider)
+	{
+		Debug.WriteLine("In initialize");
 
-        using (var context = new D2RItemsContext(
-            serviceProvider.GetRequiredService<
-                DbContextOptions<D2RItemsContext>>()))
-        {
-            if (context == null || context.Weapon == null)
-            {
-                throw new ArgumentNullException("Null D2RItemsContext");
-            }
+		using (var context = new D2RItemsContext(
+			serviceProvider.GetRequiredService<
+				DbContextOptions<D2RItemsContext>>()))
+		{
+			if (context == null || context.Weapon == null)
+			{
+				throw new ArgumentNullException("Null D2RItemsContext");
+			}
 
-            if (context.Weapon.Any())
-            {
-                return;
-            }
+			if (context.Weapon.Any())
+			{
+				return;
+			}
 
-            // read JSON directly from a file
-            using (StreamReader file = File.OpenText(@"c:\users\qbdth\repos\d2r-items\wwwroot\data\weapons.json"))
-            using (JsonTextReader reader = new JsonTextReader(file))
-            {
-                Debug.WriteLine("File was loaded" + file);
-                JObject o2 = (JObject)JToken.ReadFrom(reader);
-                foreach (var j in o2)
-                {
-                    Debug.WriteLine(j);
-                }
-            }
+			// read JSON directly from a file
+			using (StreamReader file = File.OpenText(@"c:\users\qbdth\repos\d2r-items\wwwroot\data\weapons.json"))
+			using (JsonTextReader reader = new JsonTextReader(file))
+			{
+				Debug.WriteLine("File was loaded" + file);
+				JObject weaponsJson = (JObject)JToken.ReadFrom(reader);
 
-            //context.Weapon.AddRange(
+				foreach (var item in weaponsJson)
+				{
+					Debug.WriteLine((string)item.Value["name"]);
 
-            //    // Map each parameter to a new Weapon
-            //    new Weapon()
-            //    {
-            //        Name = "The Crungis",
-            //        Tier = "Elite",
-            //        Type = "Boulder",
-            //        Sockets = 5,
-            //        OneHandDmg = 15,
-            //        TwoHandDmg = 150
-            //    }
-            //    );
-            //context.SaveChanges();
-        }
-    }
+					int twoHandDmg = 0;
+					if (item.Value["2handmaxdam"] != null)
+					{
+						twoHandDmg = (int)item.Value["2handmaxdam"];
+					}
+					else if (item.Value["maxmisdam"] != null)
+					{
+						twoHandDmg = (int)item.Value["maxmisdam"];
+					}
+
+					context.Weapon.Add(
+						new Weapon()
+						{
+							Name = (string)item.Value["name"],
+							Tier = "Elite",
+							Type = Weapon.WeaponTypeTags[(string)item.Value["type"]][0],
+							Sockets = item.Value["gemsockets"] == null ? 0 : (int)item.Value["gemsockets"],
+							OneHandDmg = item.Value["maxdam"] == null ? 0 : (int)item.Value["maxdam"],
+							TwoHandDmg = twoHandDmg,
+							Speed = item.Value["speed"] == null ? 0 : (int)item.Value["speed"]
+						});
+				}
+			}
+
+			//context.Weapon.AddRange(
+
+			//    // Map each parameter to a new Weapon
+			//    new Weapon()
+			//    {
+			//        Name = "The Crungis",
+			//        Tier = "Elite",
+			//        Type = "Boulder",
+			//        Sockets = 5,
+			//        OneHandDmg = 15,
+			//        TwoHandDmg = 150
+			//    }
+			//    );
+			context.SaveChanges();
+		}
+	}
 }
