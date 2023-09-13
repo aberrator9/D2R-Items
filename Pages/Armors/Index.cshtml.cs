d8b8c966e4e5500ea346a6fa7c1ec79ed66451a2
Justin Linton <127802772+aberrator9@users.noreply.github.com>
2023-09-12 22:08:11 -0400

    Add player class filtering.

--- Pages/Armors/Index.cshtml.cs
+++ Pages/Armors/Index.cshtml.cs
@@ -9,122 +9,146 @@ using D2RItems.Data;
 using D2RItems.Models;
 using System.Diagnostics;
 using System.IO.IsolatedStorage;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Options;
 
 namespace D2RItems.Pages.Armors
 {
-    public class IndexModel : PageModel
-    {
-        private readonly D2RItems.Data.D2RItemsContext _context;
+	public class IndexModel : PageModel
+	{
+		private readonly D2RItems.Data.D2RItemsContext _context;
 
-        public IndexModel(D2RItems.Data.D2RItemsContext context)
-        {
-            _context = context;
-        }
+		public IndexModel(D2RItems.Data.D2RItemsContext context)
+		{
+			_context = context;
+		}
 
-        public string NameSort { get; set; }
-        public string TierSort { get; set; }
-        public string SlotSort { get; set; }
-        public string SocketsSort { get; set; }
-        public string ClassSort { get; set; }
-        public string WeightSort { get; set; }
-        public string ReqStrSort { get; set; }
-        public string CurrentSort { get; set; }
-        public string CurrentFilter { get; set; }
+		public string NameSort { get; set; }
+		public string TierSort { get; set; }
+		public string SlotSort { get; set; }
+		public string SocketsSort { get; set; }
+		public string ClassSort { get; set; }
+		public string WeightSort { get; set; }
+		public string ReqStrSort { get; set; }
+		public string CurrentSort { get; set; }
+		public string CurrentFilter { get; set; }
 
-        public IList<Armor> Armors { get; set; } = default!;
+		[BindProperty]
+		public string SelectedClass { get; set; }
+		public SelectList ClassSelectList { get; set; }
+		private readonly string[] classes ={
+			//"Amazon",
+			//"Assassin",
+			"Barbarian",
+			"Druid",
+			"Paladin",
+			"Necro",
+			//"Sorceress",
+			};
 
-        public async Task OnGetAsync(string sortOrder, string searchString)
-        {
-            if (_context.Armors == null)
-            {
-                Debug.WriteLine("Armors context null");
-                return;
-            }
+		public IList<Armor> Armors { get; set; } = default!;
 
-            CurrentSort = sortOrder;
-            NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
-            SlotSort = sortOrder == "Slot" ? "slot_desc" : "Slot";
-            TierSort = sortOrder == "Tier" ? "tier_desc" : "Tier";
-            SocketsSort = sortOrder == "Sockets" ? "sockets_desc" : "Sockets";
-            ClassSort = sortOrder == "Class" ? "class_desc" : "Class";
-            WeightSort = sortOrder == "Weight" ? "weight_desc" : "Weight";
-            ReqStrSort = sortOrder == "ReqStr" ? "req_str_desc" : "ReqStr";
+		public async Task OnGetAsync(string sortOrder, string searchString, string selectedClass)
+		{
+			if (_context.Armors == null)
+			{
+				Debug.WriteLine("Armors context null");
+				return;
+			}
 
-            CurrentFilter = searchString;
+			ClassSelectList = new SelectList(classes.ToList());
+            SelectedClass = selectedClass;
+			Debug.WriteLine("Selected class is " + SelectedClass);
 
-            IQueryable<Armor> armors = from a in _context.Armors
-                                       select a;
+			CurrentSort = sortOrder;
+			NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+			SlotSort = sortOrder == "Slot" ? "slot_desc" : "Slot";
+			TierSort = sortOrder == "Tier" ? "tier_desc" : "Tier";
+			SocketsSort = sortOrder == "Sockets" ? "sockets_desc" : "Sockets";
+			ClassSort = sortOrder == "Class" ? "class_desc" : "Class";
+			WeightSort = sortOrder == "Weight" ? "weight_desc" : "Weight";
+			ReqStrSort = sortOrder == "ReqStr" ? "req_str_desc" : "ReqStr";
 
-            if (!String.IsNullOrEmpty(searchString))
-            {
-                if (searchString.Contains("*"))
-                {
-                    if (searchString.StartsWith("*"))
-                    {
-                        armors = from a in _context.Armors
-                                 where a.Name.EndsWith(searchString.Substring(1, searchString.Length - 1))
-                                 select a;
-                    }
-                    else if (searchString.EndsWith("*"))
-                    {
-                        armors = from a in _context.Armors
-                                 where a.Name.StartsWith(searchString.Substring(0, searchString.Length - 1))
-                                 select a;
-                    }
-                }
-                else
-                {
-                    armors = armors.Where(a => a.Name.Contains(searchString));
-                }
-            }
+			CurrentFilter = searchString;
 
-            switch (sortOrder)
-            {
-                case "name_desc":
-                    armors = armors.OrderByDescending(a => a.Name);
-                    break;
-                case "Slot":
-                    armors = armors.OrderBy(a => a.Slot);
-                    break;
-                case "slot_desc":
-                    armors = armors.OrderByDescending(a => a.Slot);
-                    break;
-                case "Tier":
-                    armors = armors.OrderBy(w => w.Tier);
-                    break;
-                case "tier_desc":
-                    armors = armors.OrderByDescending(a => a.Tier);
-                    break;
-                case "Sockets":
-                    armors = armors.OrderBy(a => a.Sockets);
-                    break;
-                case "sockets_desc":
-                    armors = armors.OrderByDescending(a => a.Sockets);
-                    break;
-                case "Weight":
-                    armors = armors.OrderBy(a => a.Weight);
-                    break;
-                case "weight_desc":
-                    armors = armors.OrderByDescending(a => a.Weight);
-                    break;
-                case "ReqStr":
-                    armors = armors.OrderBy(w => w.ReqStr);
-                    break;
-                case "req_str_desc":
-                    armors = armors.OrderByDescending(w => w.ReqStr);
-                    break;
-                case "Class":
-                    armors = armors.OrderBy(w => w.Class);
-                    break;
-                case "class_desc":
-                    armors = armors.OrderByDescending(w => w.Class);
-                    break;
-                default:
-                    armors = armors.OrderBy(w => w.Name);
-                    break;
-            }
+			IQueryable<Armor> armors = from a in _context.Armors
+									   select a;
 
-            Armors = await armors.AsNoTracking().ToListAsync();
-        }
-    }
+			if (!String.IsNullOrEmpty(searchString))
+			{
+				if (searchString.Contains("*"))
+				{
+					if (searchString.StartsWith("*"))
+					{
+						armors = from a in _context.Armors
+								 where a.Name.EndsWith(searchString.Substring(1, searchString.Length - 1))
+								 select a;
+					}
+					else if (searchString.EndsWith("*"))
+					{
+						armors = from a in _context.Armors
+								 where a.Name.StartsWith(searchString.Substring(0, searchString.Length - 1))
+								 select a;
+					}
+				}
+				else
+				{
+					armors = armors.Where(a => a.Name.Contains(searchString));
+				}
+			}
+
+			if(!String.IsNullOrEmpty(SelectedClass))
+			{
+			    armors = armors.Where(a=>a.Slot.Contains(SelectedClass));
+			}
+
+			switch (sortOrder)
+			{
+				case "name_desc":
+					armors = armors.OrderByDescending(a => a.Name);
+					break;
+				case "Slot":
+					armors = armors.OrderBy(a => a.Slot);
+					break;
+				case "slot_desc":
+					armors = armors.OrderByDescending(a => a.Slot);
+					break;
+				case "Tier":
+					armors = armors.OrderBy(w => w.Tier);
+					break;
+				case "tier_desc":
+					armors = armors.OrderByDescending(a => a.Tier);
+					break;
+				case "Sockets":
+					armors = armors.OrderBy(a => a.Sockets);
+					break;
+				case "sockets_desc":
+					armors = armors.OrderByDescending(a => a.Sockets);
+					break;
+				case "Weight":
+					armors = armors.OrderBy(a => a.Weight);
+					break;
+				case "weight_desc":
+					armors = armors.OrderByDescending(a => a.Weight);
+					break;
+				case "ReqStr":
+					armors = armors.OrderBy(w => w.ReqStr);
+					break;
+				case "req_str_desc":
+					armors = armors.OrderByDescending(w => w.ReqStr);
+					break;
+				case "Class":
+					armors = armors.OrderBy(w => w.Class);
+					break;
+				case "class_desc":
+					armors = armors.OrderByDescending(w => w.Class);
+					break;
+				default:
+					armors = armors.OrderBy(w => w.Name);
+					break;
+			}
+
+			Armors = await armors.AsNoTracking().ToListAsync();
+		}
+	}
 }
