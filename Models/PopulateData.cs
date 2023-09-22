9bdaef3ae2ac48e4f706c6836512d7f4eea953c1
Justin Linton <127802772+aberrator9@users.noreply.github.com>
2023-09-22 18:50:29 -0400

    Add environment as service to fix WebRootPath using local path instead of server path.

--- Models/PopulateData.cs
+++ Models/PopulateData.cs
@@ -5,125 +5,117 @@ using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.Diagnostics;
+using static System.Formats.Asn1.AsnWriter;
 
 namespace D2RItems.Models;
 
 public class PopulateData
 {
-	public PopulateData(IWebHostEnvironment webHostEnvironment)
-	{
-		WebHostEnvironment = webHostEnvironment;
-	}
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var context = new D2RItemsContext(serviceProvider.GetRequiredService<DbContextOptions<D2RItemsContext>>()))
+        {
+            if (context == null || context.Weapons == null || context.Armors == null)
+            {
+                throw new ArgumentNullException("Null D2RItemsContext");
+            }
 
-	public IWebHostEnvironment WebHostEnvironment { get; }
+            IWebHostEnvironment webHostEnvironment = serviceProvider.GetService<IWebHostEnvironment>();
 
-	public static void Initialize(IServiceProvider serviceProvider)
-	{
-		using (var context = new D2RItemsContext(
-			serviceProvider.GetRequiredService<
-				DbContextOptions<D2RItemsContext>>()))
-		{
-			if (context == null || context.Weapons == null || context.Armors == null)
-			{
-				throw new ArgumentNullException("Null D2RItemsContext");
-			}
+            if (!context.Weapons.Any())
+            {
+                using StreamReader file = File.OpenText(Path.Combine(webHostEnvironment.WebRootPath, "data", "weapons.json"));
+                using JsonTextReader reader = new JsonTextReader(file);
 
-			if (!context.Weapons.Any())
-			{
-				using (StreamReader file = File.OpenText(@"c:\users\qbdth\repos\d2r-items\wwwroot\data\weapons.json"))
-				using (JsonTextReader reader = new JsonTextReader(file))
-				{
-					JObject weaponsJson = (JObject)JToken.ReadFrom(reader);
+                JObject weaponsJson = (JObject)JToken.ReadFrom(reader);
+                foreach (var item in weaponsJson)
+                {
+                    int twoHandDmg = 0;
+                    if (item.Value["2handmaxdam"] != null)
+                    {
+                        twoHandDmg = (int)item.Value["2handmaxdam"];
+                    }
+                    else if (item.Value["maxmisdam"] != null)
+                    {
+                        twoHandDmg = (int)item.Value["maxmisdam"];
+                    }
 
-					foreach (var item in weaponsJson)
-					{
-						int twoHandDmg = 0;
-						if (item.Value["2handmaxdam"] != null)
-						{
-							twoHandDmg = (int)item.Value["2handmaxdam"];
-						}
-						else if (item.Value["maxmisdam"] != null)
-						{
-							twoHandDmg = (int)item.Value["maxmisdam"];
-						}
-
-						string tier = "Normal";
-						if (item.Key == (string)item.Value["ubercode"])
-						{
-							tier = "Exceptional";
-						}
-						else if (item.Key == (string)item.Value["ultracode"])
-						{
-							tier = "Elite";
-						}
+                    string tier = "Normal";
+                    if (item.Key == (string)item.Value["ubercode"])
+                    {
+                        tier = "Exceptional";
+                    }
+                    else if (item.Key == (string)item.Value["ultracode"])
+                    {
+                        tier = "Elite";
+                    }
 
 
-						context.Weapons.Add(
-							new Weapon()
-							{
-								Name = (string)item.Value["name"],
-								Tier = tier,
-								Type = Weapon.WeaponTypeTags[(string)item.Value["type"]][0],
-								Sockets = item.Value["gemsockets"] == null ? 0 : (int)item.Value["gemsockets"],
-								OneHandDmg = item.Value["maxdam"] == null ? 0 : (int)item.Value["maxdam"],
-								TwoHandDmg = twoHandDmg,
-								Speed = item.Value["speed"] == null ? 0 : (int)item.Value["speed"]
-							});
-					}
-				}
-			}
+                    context.Weapons.Add(
+                        new Weapon()
+                        {
+                            Name = (string)item.Value["name"],
+                            Tier = tier,
+                            Type = Weapon.WeaponTypeTags[(string)item.Value["type"]][0],
+                            Sockets = item.Value["gemsockets"] == null ? 0 : (int)item.Value["gemsockets"],
+                            OneHandDmg = item.Value["maxdam"] == null ? 0 : (int)item.Value["maxdam"],
+                            TwoHandDmg = twoHandDmg,
+                            Speed = item.Value["speed"] == null ? 0 : (int)item.Value["speed"]
+                        });
+                }
+            }
 
-			if (!context.Armors.Any())
-			{
-				using (StreamReader file = File.OpenText(@"c:\users\qbdth\repos\d2r-items\wwwroot\data\armor.json"))
-				using (JsonTextReader reader = new JsonTextReader(file))
-				{
-					JObject armorJson = (JObject)JToken.ReadFrom(reader);
+            if (!context.Armors.Any())
+            {
+                using (StreamReader file = File.OpenText(Path.Combine(webHostEnvironment.WebRootPath, "data", "armor.json")))
+                using (JsonTextReader reader = new JsonTextReader(file))
+                {
+                    JObject armorJson = (JObject)JToken.ReadFrom(reader);
 
-					foreach (var item in armorJson)
-					{
-						string tier = "Normal";
-						if (item.Key == (string)item.Value["ubercode"])
-						{
-							tier = "Exceptional";
-						}
-						else if (item.Key == (string)item.Value["ultracode"])
-						{
-							tier = "Elite";
-						}
+                    foreach (var item in armorJson)
+                    {
+                        string tier = "Normal";
+                        if (item.Key == (string)item.Value["ubercode"])
+                        {
+                            tier = "Exceptional";
+                        }
+                        else if (item.Key == (string)item.Value["ultracode"])
+                        {
+                            tier = "Elite";
+                        }
 
-						string slot = (string)item.Value["type"] switch
-						{
-							"tors" => "Torso",
-							"shie" => "Shield",
-							"ashd" => "Shield [Paladin]",
-							"head" => "Offhand [Necro]",
-							"glov" => "Gloves",
-							"boot" => "Boots",
-							"belt" => "Belt",
-							"helm" => "Helm",
-							"circ" => "Helm",
-							"phlm" => "Helm [Barbarian]",
-							"pelt" => "Helm [Druid]",
-							_ => throw new Exception("Unaccounted armor slot type")
-						};
+                        string slot = (string)item.Value["type"] switch
+                        {
+                            "tors" => "Torso",
+                            "shie" => "Shield",
+                            "ashd" => "Shield [Paladin]",
+                            "head" => "Offhand [Necro]",
+                            "glov" => "Gloves",
+                            "boot" => "Boots",
+                            "belt" => "Belt",
+                            "helm" => "Helm",
+                            "circ" => "Helm",
+                            "phlm" => "Helm [Barbarian]",
+                            "pelt" => "Helm [Druid]",
+                            _ => throw new Exception("Unaccounted armor slot type")
+                        };
 
 
-						context.Armors.Add(
-							new Armor()
-							{
-								Name = (string)item.Value["name"],
-								Tier = tier,
-								Slot = slot,
-								Sockets = item.Value["gemsockets"] == null ? 0 : (int)item.Value["gemsockets"],
-								Class = item.Value["maxac"] == null ? 0 : (int)item.Value["maxac"],
-								Weight = "Light",
-								ReqStr = item.Value["reqstr"] == null ? 0 : (int)item.Value["reqstr"]
-							});
-					}
-				}
-			}
-			context.SaveChanges();
-		}
-	}
+                        context.Armors.Add(
+                            new Armor()
+                            {
+                                Name = (string)item.Value["name"],
+                                Tier = tier,
+                                Slot = slot,
+                                Sockets = item.Value["gemsockets"] == null ? 0 : (int)item.Value["gemsockets"],
+                                Class = item.Value["maxac"] == null ? 0 : (int)item.Value["maxac"],
+                                Weight = "Light",
+                                ReqStr = item.Value["reqstr"] == null ? 0 : (int)item.Value["reqstr"]
+                            });
+                    }
+                }
+            }
+            context.SaveChanges();
+        }
+    }
 }
--- Program.cs
+++ Program.cs
@@ -3,21 +3,23 @@ using Microsoft.Extensions.DependencyInjection;
 using D2RItems.Data;
 using D2RItems.Models;
 using System.Diagnostics;
+using Microsoft.Extensions.Hosting.Internal;
 
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<D2RItemsContext>(options =>
 	options.UseSqlServer(builder.Configuration.GetConnectionString("D2RItemsContext") ?? throw new InvalidOperationException("Connection string 'D2RItemsContext' not found.")));
 
+builder.Services.AddSingleton(builder.Environment);
+
 var app = builder.Build();
 
 using (var scope = app.Services.CreateScope())
 {
-	var services = scope.ServiceProvider;
+    var services = scope.ServiceProvider;
 
-	PopulateData.Initialize(services);
+    PopulateData.Initialize(services);
 }
 
 // Configure the HTTP request pipeline.
